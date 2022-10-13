a278752985dfbf4cc7874f7074374e7dbd9037c8
Muhammad Kamran Syed <s.muhammadkamran@gmail.com>
2022-10-13 06:19:13 +0300

    Update SupplyChain.sol

--- SupplyChainApp/contracts/SupplyChain.sol
+++ SupplyChainApp/contracts/SupplyChain.sol
@@ -7,8 +7,8 @@ contract SupplyChain {
   uint32 public participant_id = 0;
   uint32 public owner_id = 0;
 
-  constructor() {
-  }
+  //constructor() {
+  //}
 
   struct product {
     string modelNumber;
